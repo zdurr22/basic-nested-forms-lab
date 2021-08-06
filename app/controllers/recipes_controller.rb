e6889f67696d891af7c676eb919d7f72cdfb3355
zdurr22 <zdurr22@gmail.com>
2021-08-06 10:33:49 -0400

    commit tree 7cf55e675892e8a0ea4ffebf545a51da48339468

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -15,7 +15,7 @@ class RecipesController < ApplicationController
 
   def create
     recipe = Recipe.create(recipe_params)
-    redirect_to recipe_path
+    redirect_to recipes_path
   end
 
   private
